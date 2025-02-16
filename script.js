043733366ddd028d8a01d094300a061e87283ec9
KBormon <kishorbormon121@gmail.com>
2025-02-17 01:15:59 +0600

    Merge remote changes

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-// Spider effect when mouse moves
-document.addEventListener("mousemove", function(e) {
-    let spider = document.createElement("div");
-    spider.classList.add("spider");
-    document.body.appendChild(spider);
-
-    spider.style.left = e.clientX + "px";
-    spider.style.top = e.clientY + "px";
-
-    setTimeout(() => {
-        spider.remove(); // Remove after animation
-    }, 500);
-});
+// Simple Console Log
+console.log("Welcome to Kishor Bormon's portfolio website!");
